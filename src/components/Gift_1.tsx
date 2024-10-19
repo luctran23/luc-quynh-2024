7bf204f4256c871370648618dff8bcef30865fba
Luc.Tran <anhvanluc1999@gmail.com>
2024-10-19 13:10:03 +0700

    spell check

--- src/components/Gift_1.tsx
+++ src/components/Gift_1.tsx
@@ -23,7 +23,7 @@ export const Gift_1 = () => {
             <Title>Ôi không! Đây lại là một món quà bí ẩn. 😭</Title>
             <img src={"me/IMG_4574.JPG"} className="GF_01" alt="GF_01" />
             <SubText>
-                {`vợ yêu dấu, a  muốn gửi tặng e món quà nhỏ này kèm lời nhắn. Chúc vợ yêu luôn vui vẻ, luôn hạnh phục và ngày càng xinh đẹp, học tập tốt , hoàn thành được những dự định của e để về với a nha. Tuy anh không thể đưa món quà này tận tay vợ nhưng nó duy nhất chỉ dành cho vợ thôi. Tết về anh trao tận nơi nha.kkk 😊
+                {`vợ yêu dấu, a  muốn gửi tặng e món quà nhỏ này kèm lời nhắn. Chúc vợ yêu luôn vui vẻ, luôn hạnh phúc và ngày càng xinh đẹp, học tập tốt , hoàn thành được những dự định của e để về với a nha. Tuy anh không thể đưa món quà này tận tay vợ nhưng nó duy nhất chỉ dành cho vợ thôi. Tết về anh trao tận nơi nha.kkk 😊
 `}
             </SubText>
         </GiftWrapper>
